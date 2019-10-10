cb5cb55594779b128622e1a3c069116993b26900
Carlos Abris <cmabris@gmail.com>
2019-10-10 14:14:07 +0000

    Mejora 2

--- public/tema6/index.php
+++ public/tema6/index.php
@@ -6,23 +6,44 @@
 </head>
 <body>
 	<h1>Formularios Usables</h1>
-	<?php if ( ! $_POST ) { 
+	<?php 
+
+	function showErrors($errors) {
+		echo '<ul>';
+		foreach ($errors as $error) {
+			echo '<li>' . $error . '</li>';
+		}
+		echo '</ul><br>';
+	}
+
+	if ( ! $_POST ) { 
 		include 'formulario.php';
 	} else {
+		$errors = [];
 		if ( ! isset($_POST['name'])) {
-			echo 'No he recibido el nombre';
-		} elseif (strlen($_POST['name']) < 3){
-			echo 'El nombre es demasiado corto';
-		} elseif ( ! isset($_POST['email'])) {
-			echo 'No he recibido el email';
-		} elseif (strlen($_POST['email']) < 6) {
-			echo 'El correo electrónico no es válido';
-		} elseif ( ! isset($_POST['password1']) || ! isset($_POST['password2'])) {
-			echo 'No he recibido ambas contraseñas';
-		} elseif ( strlen($_POST['password1']) < 6) {
-			echo 'La contraseña debe tener al menos 6 caracteres';
-		} elseif ($_POST['password1'] != $_POST['password2']) {
-			echo 'Ambas contraseñas deben ser iguales';
+			$errors[] = 'No he recibido el nombre';
+		}
+		if (strlen($_POST['name']) < 3){
+			$errors[] = 'El nombre es demasiado corto';
+		} 
+		if ( ! isset($_POST['email'])) {
+			$errors[] = 'No he recibido el email';
+		}
+		if (strlen($_POST['email']) < 6) {
+			$errors[] = 'El correo electrónico no es válido';
+		} 
+		if ( ! isset($_POST['password1']) || ! isset($_POST['password2'])) {
+			$errors[] = 'No he recibido ambas contraseñas';
+		} 
+		if ( strlen($_POST['password1']) < 6) {
+			$errors[] = 'La contraseña debe tener al menos 6 caracteres';
+		} 
+		if ($_POST['password1'] != $_POST['password2']) {
+			$errors[] = 'Ambas contraseñas deben ser iguales';
+		} 
+		if ($errors) {
+			showErrors($errors);
+			echo '<br><a href="index.php">Volver al formulario</a>';
 		} else {
 			echo 'Todo bien, pasamos a registrar ese usuario';
 		}
@@ -30,3 +51,4 @@
 	} ?>
 </body>
 </html>
+
